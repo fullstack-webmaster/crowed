e69bc297712ea089efd48ea653879acd28a63f79
webmaster <web.starlightmaster@gmail.com>
2017-01-04 11:41:41 +0800

    Home/Home routing compoennt.

---
+++ src/containers/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import AppLayout from '../../layouts';
+/**
+ * class Home
+ */
+export default class Home extends React.Component {
+  /**
+   * @return {XML}
+   */
+  render() {
+    return (
+      <AppLayout>
+        <div>Welcome,</div>
+      </AppLayout>
+    );
+  }
+}
